de46166b35c2aed4c3755e228028716b30dfe977
owaisnadeem18 <owaisnadeem15@gmail.com>
2025-04-10 23:12:11 +0500

    started learning about redux baic concepts

---
+++ index.js
@@ -0,0 +1,24 @@
+// Refresher of Redux in this file:
+
+import {createStore} from "redux"
+
+
+const initialValue = {
+    amount: 0
+}
+
+const Deposit = (state= initialValue , action) => {
+    const {type , payLoad} = action
+    if (type == "deposit") {
+        return {amount: state.amount + payLoad}
+    } 
+    
+    return state
+}
+
+const bankStore = createStore(Deposit)
+
+
+bankStore.dispatch({type: "deposit" , payLoad: 1000})
+
+console.log(bankStore.getState())
